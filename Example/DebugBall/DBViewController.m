47053e1a5a22180a2add44aa46131983cae110fb
jocer <ghji@i9i8.com>
2018-03-20 21:07:24 +0800

    alter project pbxproj

--- Example/DebugBall/DBViewController.m
+++ Example/DebugBall/DBViewController.m
@@ -18,21 +18,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [DebugManager registerUserDataWithUserID:@"1835724623" userName:@"ghji@i9i8.com" userToken:@"1351-5t5y-326-63423"];
-    
-    [DebugManager registerPushToken:@"XXXX-XXXX-XXXX-XXXX-XXXX"];
-    
-    [DebugManager registerNotification:kAPIHostDidChangedNotification byHandler:^(NSDictionary *info) {
-        NSLog(@"%@",info);
-        self.tableView = [UITableView new];
-    }];
-    
-    [DebugManager registerNotification:kH5APIHostDidChangedNotification byHandler:^(NSDictionary *info) {
-        NSLog(@"%@",info);
-    }];
-    [DebugManager registerDefaultAPIHosts:@[@"www.ww.ww"] andH5APIHosts:@[@"xxx.xxx.xxx"]];
-    
-    [DebugManager installDebugViewByDefault];
+//    [DebugManager registerUserDataWithUserID:@"1835724623" userName:@"ghji@i9i8.com" userToken:@"1351-5t5y-326-63423"];
+//    
+//    [DebugManager registerPushToken:@"XXXX-XXXX-XXXX-XXXX-XXXX"];
+//    
+//    [DebugManager registerNotification:kAPIHostDidChangedNotification byHandler:^(NSDictionary *info) {
+//        NSLog(@"%@",info);
+//        self.tableView = [UITableView new];
+//    }];
+//    
+//    [DebugManager registerNotification:kH5APIHostDidChangedNotification byHandler:^(NSDictionary *info) {
+//        NSLog(@"%@",info);
+//    }];
+//    [DebugManager registerDefaultAPIHosts:@[@"www.ww.ww"] andH5APIHosts:@[@"xxx.xxx.xxx"]];
+//    
+//    [DebugManager installDebugViewByDefault];
 
 }
 
